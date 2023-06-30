a2a342930df3fe7330033112e98dc9886a524d54
Bikram Bhusal <bikrambhusal75@gmail.com>
2023-06-30 23:52:40 +0545

    Practice Session 1

--- practice.js
+++ practice.js
@@ -3,7 +3,7 @@
 // WAP in JS to generate a random number and store it in a variable.
 // The program then takes an input from the user to tell them whether
 // the guess was correct, greater or lesser than the original number.
-// Guessing limit should be only 10
+// Guessing limit should be only 15
 
 let generator = Math.round(Math.random()*100)+1;//Math.random generates a random number betn 0 and 1.
 //Math.round returns a supplied nearest integer
@@ -22,12 +22,12 @@ do{
   console.log(result);
   count++;
 
-  if(count>9){
+  if(count>14){
     alert("Sorry, You have reached the limit for today!");
   };
   if(user_input == generator){
-    count = 10;
+    count = 15;
 }
-}while(count<10);
+}while(count<15);
 
 
