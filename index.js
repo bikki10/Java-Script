52c3bce9a18e428078a710bf63e9087b68dd32bf
Bikki <bikrambhusal75@gmail.com>
2023-07-31 14:00:03 +0545

    Learned about DOM, BOM and Window Object.

---
+++ index.js
@@ -0,0 +1,13 @@
+// Lets learn about DOM, BOM and Window Objects in JS
+
+// DOM => Document Object Model
+
+// window.console.log(window);
+// console.log(document.body);
+// document.body.style.background = "";
+
+// BOM => Browser Object Model
+// alert, prompt, etc functions are the part of browser object model
+// use location.href = "url" which will rerdirect you to the website.
+
+
--- practice.js
+++ practice.js
@@ -5,29 +5,31 @@
 // the guess was correct, greater or lesser than the original number.
 // Guessing limit should be only 15
 
-let generator = Math.round(Math.random()*100)+1;//Math.random generates a random number betn 0 and 1.
-//Math.round returns a supplied nearest integer
-let count = 0;
-console.log(generator);
+// let generator = Math.round(Math.random()*100)+1;//Math.random generates a random number betn 0 and 1.
+// //Math.round returns a supplied nearest integer
+// let count = 0;
+// console.log(generator);
 
-do{
-  let user_input = prompt("What's your guess? ");
-  user_input = Number.parseInt(user_input);
+// do{
+//   let user_input = prompt("What's your guess? ");
+//   user_input = Number.parseInt(user_input);
 
-  let result = (user_input == generator) ? "\nHoly moly! You're goddamn right!\n"
-  : (user_input > generator) ? "\nYour expectations are high!\n"
-  : (user_input < generator) ? "\nYour expectations are low!\n"
-  : "Invalid character!";
+//   let result = (user_input == generator) ? "\nHoly moly! You're goddamn right!\n"
+//   : (user_input > generator) ? "\nYour expectations are high!\n"
+//   : (user_input < generator) ? "\nYour expectations are low!\n"
+//   : "Invalid character!";
 
-  console.log(result);
-  count++;
+//   console.log(result);
+//   count++;
 
-  if(count>14){
-    alert("Sorry, You have reached the limit for today!");
-  };
-  if(user_input == generator){
-    count = 15;
-}
-}while(count<15);
+//   if(count>14){
+//     alert("Sorry, You have reached the limit for today!");
+//   };
+//   if(user_input == generator){
+//     count = 15;
+// }
+// }while(count<15);
+
+
 
 
